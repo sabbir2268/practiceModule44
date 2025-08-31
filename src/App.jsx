dbe522ad9398d0b95614b2d140ad18c67f3460c6
Md Sabbir Rahman <mdsabbirrahman2268@gmail.com>
2025-08-31 12:33:57 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-center mt-20 font-bold">
+        <h1>Welcome to React template</h1>
+        <h1>Tailwind + daisyUI + fonts</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
